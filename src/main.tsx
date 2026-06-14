04043e445fd2020edce1ef695fa067373d2f15c7
AstirisAQW <deysolong.josephjose457@gmail.com>
2026-06-14 16:02:20 +0800

    things are working

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Minesweeper from './Minesweeper.tsx'
 
+import Minesweeper from './Minesweeper'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Minesweeper />
